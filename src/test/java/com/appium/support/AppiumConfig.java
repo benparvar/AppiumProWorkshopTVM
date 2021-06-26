839455e9e4d7e8fb081630f0def9b1cca97cab4b
Juliana Albuquerque de Oliveira <CIANDT\julianaalbuquerque@lnb028742cps.ciandt.global>
2021-06-26 00:22:05 -0300

    Estruturando o projeto

--- src/test/java/com/appium/support/AppiumConfig.java
+++ src/test/java/com/appium/support/AppiumConfig.java
@@ -1,21 +1,17 @@
-package com.appium.sanity;
+package com.appium.support;
+
 
 import io.appium.java_client.android.AndroidDriver;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.net.MalformedURLException;
 import java.net.URL;
 
-public class AppiumHelloWorld {
-
+public class AppiumConfig {
     private AndroidDriver driver;
     private String APP = "https://github.com/ju-padawan/apkTeste/releases/download/apkTeste/calculator-7-8-271241277.apk";
 
-    @Before
-    public void setUp() throws MalformedURLException {
+    public void start() throws MalformedURLException {
         DesiredCapabilities capabilities = new DesiredCapabilities();
 
         capabilities.setCapability("platformName", "Android");
@@ -26,20 +22,9 @@ public class AppiumHelloWorld {
         driver = new AndroidDriver(new URL("http://localhost:4723/wd/hub"), capabilities);
     }
 
-    @After
-    public void tearDown() {
+    public void stop() {
         if (driver != null) {
             driver.quit();
         }
-    }
-
-    @Test
-    public void appiumHelloWorld() throws InterruptedException {
-        Thread.sleep(10000);
-        driver.findElementById("com.google.android.calculator:id/digit_9").click();
-        driver.findElementByAccessibilityId("multiply").click();
-        driver.findElementById("com.google.android.calculator:id/digit_2").click();
-        driver.findElementByAccessibilityId("equals").click();
-        Thread.sleep(10000);
     }
 }
